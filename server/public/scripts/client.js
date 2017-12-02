2478f7d6d94f2e790f4ed1cb04f89cd577d8e949
David Clark <primecomp@Davids-MacBook-Pro.local>
2017-12-01 18:13:53 -0600

    lots done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,37 @@ myApp.controller('PictureController', ['$http', function($http){
     console.log('picture controller has been loaded');
     var self = this;
     self.message = 'Get your picutres up';
+
+    self.picturesArray = [
+        {
+        image: "./images/Sydney_Harbour_Bridge.jpg",
+        story: `I lived in Sydney right next to the bridge for 
+        several months and had the time of my life. I made a ton of great friends and memories, 
+        inculding scubba diving on the great barrier reef and bundgy jumping in a rainforest.`,
+        show: false,
+        like: 0
+    }
+
+    ]
+
+self.addStory = function(life) {
+    if (life.show == false) {
+        life.show = true;
+    } else if (life.show == true) {
+        life.show = false;
+    }
+        life.like ++;
+     
+}
+
+
+
+
+
+
+
+
+
+
+
 }]);
